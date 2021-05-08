06f33d4a37f52d59a61270b31803bffb61150d57
hadohado <hadohado@gmail.com>
2021-05-08 11:52:51 -0700

    Initial Commit

---
+++ Memories_stford_1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memories_stford_1
+//
+//  Created by ha tuong do on 5/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
